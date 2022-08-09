6a74aefe5cb503ff22e41a1511b78e7cb2b7e2c9
Shreyas Gavhalkar <shreyas@pop-os.localdomain>
2022-08-09 16:13:34 +0530

    removed seleium's dependancy

--- scraper.py
+++ scraper.py
@@ -1,23 +1,28 @@
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
+import re
+import pprint
 
-
-options = webdriver.FirefoxOptions()
-options.add_argument("--headless")
-
-browser_instance = webdriver.Firefox(options=options)
-browser_instance.get("https://www.greentribunal.gov.in/western-zone-cause-list")
-page_source = browser_instance.page_source
+request_object = requests.get("https://www.greentribunal.gov.in/western-zone-cause-list", verify=False)
+page_source = request_object._content
 
 # print(page_source)
 
-soup = BeautifulSoup(page_source)
+soup = BeautifulSoup(page_source, features="html.parser")
 # print(soup)
 
 table = soup.find_all("table", attrs={"class":"views-table cols-2 table custm-tbl table-bordered"})
 
 links = []
+
 for i in table:
     links.append(i.find_all("a",href=True, attrs={"target":"_blank"}))
 
-print(links)
+# pprint.pprint(links[0])
+links = links[0]
+# pprint.pprint(links)
+print(dir(links[0]))
+
+
+for link in links:
+    
