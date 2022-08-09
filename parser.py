f3a357b47fb47db1e38f2a0c058d87b0aae65f34
Alok Joshi <AlokJoshi523@gmail.com>
2022-08-09 16:55:13 +0530

    Fixed bugs

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfReader
 def pdf_to_text(pdf_name):
     """ Converts the pdf into a text string """
 
-    reader = PdfReader("cl_1.pdf")
+    reader = PdfReader(pdf_name)
     text = ""
     for page in reader.pages:
         text += page.extract_text()+"\n"
